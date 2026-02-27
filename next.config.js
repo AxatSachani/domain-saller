d17c35ad3af8b8bd55209933e03984408dacdd1d
Axat Sachani <axat.sachani@gmail.com>
2026-02-27 23:15:41 +0530

    - code update

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
     NEXT_PUBLIC_DOMAIN_NAME: process.env.NEXT_PUBLIC_DOMAIN_NAME,
   },
+  output: 'export',
 };
 
 module.exports = nextConfig;
